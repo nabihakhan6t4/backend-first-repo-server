29e1432d6ff6f2ae4d7c20bb8e3fb71848d8921b
nabihakhan6t4 <khannabiha923@gmail.com>
2025-02-14 17:55:09 +0500

    or operator

--- server.mjs
+++ server.mjs
@@ -1,10 +1,10 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port =    process.env.PORT ||  3000;
 
 app.get('/', (req, res) => {
-   
+   req.ip
   res.send('Hello World!' + new Date().getDate().toString());
 });
 
